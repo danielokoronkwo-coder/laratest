086a3b3b4bd8db2b316cc8d5828a9e992ac8d640
danielokoronkwo-coder <danielokoronkwo90@gmail.com>
2022-07-09 13:18:10 +0100

    added database setup and dummy data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    $users = User::all();
+    return view('test', ['users' => $users]);
 });
